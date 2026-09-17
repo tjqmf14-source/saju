ca9a05297617f5d707c6c1e6ae5eee63d39a279b
tjqmf14-source <tjqmf14@gmail.com>
2026-09-17 20:24:29 +0900

    chore: remove obsolete test setup

--- tests/setup.js
+++
@@ -1,5 +0,0 @@
-import { createRequire } from 'node:module';
-
-const require = createRequire(import.meta.url);
-const lunarLib = require('../vendor/lunar.cjs');
-Object.assign(globalThis, lunarLib);
