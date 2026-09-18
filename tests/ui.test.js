3f4809a2d11a8b54d214c7c5be27b68357066bae
tjqmf14-source <tjqmf14@gmail.com>
2026-09-18 21:25:28 +0900

    test: align UI selectors with daily remodel

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises';
 
 const html = await readFile(new URL('../index.html', import.meta.url), 'utf8');
 
-for (const id of ['dailyFortuneGrid','todayLucky','tojungQuarterGrid','yearAdviceGrid','seasonGuide','reportNav']) {
+for (const id of ['dailyFortuneGrid','dailyPrimary','dailyMetrics','todayLucky','tojungQuarterGrid','yearAdviceGrid','reportNav']) {
   test(`premium UI contains #${id}`, () => {
     assert.match(html, new RegExp(`id=["']${id}["']`));
   });
