277d50773876bb132beac4ca00d618e9d7ea116a
tjqmf14-source <tjqmf14@gmail.com>
2026-09-19 15:45:17 +0900

    Align UI target tests with rebuilt V11 landing page

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -4,8 +4,8 @@ import { readFile } from 'node:fs/promises';
 
 const html = await readFile(new URL('../index.html', import.meta.url), 'utf8');
 
-for (const id of ['dailyFortuneGrid','dailyPrimary','dailyMetrics','todayLucky','tojungQuarterGrid','yearAdviceGrid','reportNav']) {
-  test(`premium UI contains #${id}`, () => {
+for (const id of ['input','dailyPrimary','dailyMetrics','todayLucky','tojungQuarterGrid','yearAdviceGrid','tarot','reviews','faq']) {
+  test(`V11 UI contains #${id}`, () => {
     assert.match(html, new RegExp(`id=["']${id}["']`));
   });
 }
