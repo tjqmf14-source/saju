c4abff5e9a48626b3f3384e65de383582c95243b
tjqmf14-source <tjqmf14@gmail.com>
2026-09-24 22:07:31 +0900

    fix(android-test): import multi-node text matcher

--- android/app/src/androidTest/java/kr/naesaju/personal/NativeScreenSmokeTest.kt
+++ android/app/src/androidTest/java/kr/naesaju/personal/NativeScreenSmokeTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.captureToImage
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
